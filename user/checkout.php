6b05693acc8c48fefdcf90a0baaf1dec1780c970
hecmed <hector.med27@gmail.com>
2018-11-05 17:09:27 -0800

    checkout works now

--- user/checkout.php
+++ user/checkout.php
@@ -8,8 +8,10 @@
     $data = json_decode(file_get_contents('php://input'));
     print_r($data->Shopping);
     print_r($data->Session);
-    $dataArray = [];
-    $customerID = 2;
+    // $dataArray = [];
+    // $orderId;
+    $customerID;
+    $orderID;
     // $data/json_decode values is treated as a class object, so dereference with arrows.
     // this is how you get the brand name from the first item in the shopping cart
     // $data->Shopping[0]->Brand
@@ -23,19 +25,21 @@
     //update invoice with orderid as the incremement key of ordering as long as the same key is valid.
     //this way we don't have to keep track of timestamps when checking reciept on individual prices.
     $paymentstatus = "pass";
-    $selectCustomer = "select * from CUSTOMER where CUSTOMER_ID=".$data->Session->id.";";
-    $stmt = $link->prepare("SELECT * FROM CUSTOMER WHERE CUSTOMER_ID=(?);")
-    $stmt->bind_param('i', $data->Session->id)
-    if($someQryObject = $link->query($selectCustomer)){
-        echo "it worked";
-        echo "Grabbing the customer ID value";
-        var_dump($someQryObject->fetch_all());
+    // $selectCustomer = "select * from CUSTOMER where R_ID=".$data->Session->id.";";
+    $stmt = $link->prepare("SELECT CUSTOMER_ID FROM CUSTOMER WHERE R_ID=(?);");
+    $stmt->bind_param('i', $data->Session->id);
+    
+    if($stmt->execute()){
+        $stmt->bind_result($id);
+        while($stmt->fetch()){
+            $customerID = $id;
+        }
     }
     else{
         echo "Error: " . $query . "<br>" . $link->error;
     }
-
-
+    $stmt->close();
+    var_dump($customerID);
     $stmt = $link->prepare("INSERT INTO ORDERING(CORDER_ID, PAYMENT_STATUS) VALUES (?,?)");
     $stmt->bind_param('is', $customerID, $paymentstatus);
     if($stmt->execute()) {
@@ -47,26 +51,34 @@
     $stmt->close();
 
 
-    $getLatestOrderingID = "SELECT max(ORDER_ID) from ORDERING where CORDER_ID = ".$customerID.";";
-    if($someQryObject = $link->query($getLatestOrderingID)){
-        echo "it worked";
-        while ($row = $someQryObject->fetch_array()) {
-            $dataArray[]=$row;
+    // $getLatestOrderingID = "SELECT max(ORDER_ID) from ORDERING where CORDER_ID = ".$customerID.";";
+    $stmt = $link->prepare("SELECT max(ORDER_ID) from ORDERING where CORDER_ID = (?);");
+    $stmt->bind_param('i', $customerID);
+    if($stmt->execute()){
+        $stmt->bind_result($id);
+        while($stmt->fetch()){
+            $orderID = $id;
+            var_dump($orderID);
         }
-        echo "Grabbing the latest ordering ID value";
-        print_r($dataArray[0]);
     }
     else{
         echo "Error: " . $query . "<br>" . $link->error;
     }
-    $updateInvoice = "INSERT INTO INVOICE (ITEMS_ID, OR_ID, QUANTITY, PRICE, CUS_INFO, TOTAL)"
-    ." VALUES (".$data->Shopping[0]->id.",".$dataArray[0].",".$data->Shopping[0]->Quantity.",".$data->Shopping[0]->Price.",\"...\",".$data->Total.");";
-    if(TRUE === $link->query($updateInvoice)) {
-        echo "it worked";
+    $stmt->close();
+    // $updateInvoice = "INSERT INTO INVOICE (ITEMS_ID, OR_ID, QUANTITY, PRICE, CUS_INFO, TOTAL)"
+    // ." VALUES (".$data->Shopping[0]->id.",".$dataArray[0].",".$data->Shopping[0]->Quantity.",".$data->Shopping[0]->Price.",\"...\",".$data->Total.");";
+    $stmt = $link->prepare("INSERT INTO INVOICE (ITEMS_ID, OR_ID, QUANTITY, PRICE, CUS_INFO, TOTAL)"
+    ." VALUES (?,?,?,?,\"...\",?);");
+    for ($i=0; $i<count($data->Shopping); $i++){
+        $stmt->bind_param('iiiii',$data->Shopping[$i]->id,$orderID,$data->Shopping[$i]->Quantity,$data->Shopping[$i]->Price,$data->Total);
+        if($stmt->execute()) {
+            echo "it worked";
+        }
+        else {
+            echo "Error: " . $query . "<br>" . $link->error;
+        }
     }
-    else {
-        echo "Error: " . $query . "<br>" . $link->error;
-    }
+    $stmt->close();
     // $link->query();
     // -- subitems id --- SI_NUM the unique subitem number
     // -- order id ---- CUSTOMER_ID
